e3622f9f76dfef9a2a62a3f4835a297c8f222b3f
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2020-06-26 22:05:30 +0530

    updated

---
+++ course1/Bootstrap4/conFusion/js/script.js
@@ -0,0 +1,46 @@
+
+{/* <script>
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
+</script>  */}
+
+
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($('#carouselButton').children('span').hasClass('fa-pause')){
+            $("#mycarousel").carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+
+        else {
+            $("#mycarousel").carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        
+        }
+
+        
+    });
+});
+
+
+
+$(document).ready(function() {
+    $("#login").click(function(){
+        $('#loginModal').modal('show')
+    });
+
+
+    $("#reserve").click(function(){
+        $('#reserveModal').modal('show')
+    });
+});
